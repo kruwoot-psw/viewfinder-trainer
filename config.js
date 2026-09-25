91e47bae83593489ccd49aa28e567d44646b7868
kruwoot-psw <kruwoot@skburana.ac.th>
2026-09-25 15:53:24 +0700

    เพิ่มไฟล์ระบบครั้งแรก
    เพิ่มไฟล์ระบบครั้งแรก

---
+++ config.js
@@ -0,0 +1,6 @@
+export const SB_URL = "https://qbxasubgibsnsfrkckbr.supabase.co/rest/v1/";
+export const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFieGFzdWJnaWJzbnNmcmtja2JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzMTg0MjAsImV4cCI6MjEwNTg5NDQyMH0.VMArWA-WRARw3JV1I8U3mVrC5CCfpc60IDq8Ik-HCBE";   // anon public key
+export const CONFIG = {
+  threshold: { angle1: 12, angle2: 35, roll: 10 },
+  holdFrames: 20
+};
